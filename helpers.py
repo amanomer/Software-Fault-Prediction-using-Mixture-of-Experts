4640a66a7d3c5acb26972ca031a21214772ad05c
aman_omer <amanomer1996@gmail.com>
2020-03-22 13:55:51 +0530

    1. Inclusion of NB and SVM as experts along with DT and MLP. 2. Inclusion of  Bosting and Stacking ensemble methods along with Bagging. 3. Collection of Confusion matrix values and AUC also.
    Authored by: Santosh Rathore

--- helpers.py
+++ helpers.py
@@ -1,36 +1,45 @@
 import os
 import numpy as np
 import random
+
+from mlxtend.classifier import StackingClassifier
 from sklearn import decomposition
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
-#from xgboost import XGBClassifier
+# from xgboost import XGBClassifier
 from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import StandardScaler, normalize
 from sklearn.neural_network import MLPClassifier
 from sklearn.mixture import GaussianMixture
-from sklearn.ensemble import BaggingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import BaggingClassifier, RandomForestRegressor
+from sklearn.ensemble import AdaBoostClassifier
+#from sklearn.ensemble import StackingClassifier
+from sklearn import svm
 
-def top10FusingRF(X,Y):
+
+def top10FusingRF(X, Y):
     model = RandomForestRegressor(random_state=1, max_depth=10)
-    model.fit(X,Y)
+    model.fit(X, Y)
     importances = model.feature_importances_
-    indices = np.argsort(importances)[:-10]  #indices top 10 features
-    #print(indices)
-    #print(indice.shape)
-    X = np.delete(X,indices,1)
+    indices = np.argsort(importances)[:-10]  # indices top 10 features
+    # print(indices)
+    # print(indice.shape)
+    X = np.delete(X, indices, 1)
     return X
 
-def convert_vec2matrix(Y,k):
-    y = np.zeros([Y.shape[0] , k])
+
+def convert_vec2matrix(Y, k):
+    y = np.zeros([Y.shape[0], k])
     for idx in range(Y.shape[0]):
-        y[idx,Y[idx]] = 1
+        y[idx, Y[idx]] = 1
     return y
 
+
 def reduced_features(data):
-    #var_threshold = 0.95
+    # var_threshold = 0.95
     pca = decomposition.PCA()
-    #Reduce features by 5
+    # Reduce features by 5
     pca.n_components = data.shape[1] - 5
     pca_data = pca.fit_transform(data)
     """percentage_var_explained = pca.explained_variance_ / np.sum(pca.explained_variance_)
@@ -44,26 +53,29 @@ def reduced_features(data):
     reduced_data = pca_data[: ,:reduced_features]"""
     return pca_data
 
+
 def checkvariance(X):
-    #for i in range(X.shape[1]):
-        #print(X[: ,i:i+1].var())
+    # for i in range(X.shape[1]):
+    # print(X[: ,i:i+1].var())
     print(np.corrcoef(X))
+
 
 def loaddata(dataset_name,
              sampling_required="yes",
              standardisation="yes",
              pca_reduced="yes",
              normalization="yes"):
-
     cwd = os.getcwd()
-    data = np.genfromtxt(cwd + "/Datasets/" + dataset_name +".csv", delimiter=',')
-    #data[np.isnan(data)] = 0
-    data = data[~np.isnan(data).any(axis=1)]   #Remove rows having NaN
-    
-    Number_of_instances,Attributes = data.shape
+    data = np.genfromtxt(cwd + "/Datasets/" + dataset_name + ".csv", delimiter=',')
+    # data = np.genfromtxt(cwd + dataset_name +".csv", delimiter=',')
+    print(data)
+    # data[np.isnan(data)] = 0
+    data = data[~np.isnan(data).any(axis=1)]  # Remove rows having NaN
+
+    Number_of_instances, Attributes = data.shape
     X = np.array([x[:-1] for x in data])
     y = [0 for i in range(Number_of_instances)]
-    
+
     index = 0
     for x in data:
         if int(x[-1:]) >= 1:
@@ -71,8 +83,8 @@ def loaddata(dataset_name,
         index += 1
     Y = np.array(y)
 
-    #X = top10FusingRF(X,Y)
-    
+    # X = top10FusingRF(X,Y)
+
     if sampling_required == "yes":
         X, Y = SMOTE().fit_resample(X, Y)
 
@@ -81,10 +93,10 @@ def loaddata(dataset_name,
 
     if normalization == "yes":
         X = normalize(X)
-        
+
     if pca_reduced == "yes":
         X = reduced_features(X)
-    
+
     c = ['Green' for i in range(Y.shape[0])]
     index = 0
     for x in Y:
@@ -93,30 +105,33 @@ def loaddata(dataset_name,
         index += 1
     color = np.array(c)
 
-    return X,Y,color
+    return X, Y, color
 
-#GM model based ME
-def ME_model( X_train, Y_train, X_test, n_estimator, expert_model, _DS, _LP):
 
+# GM model based ME
+def ME_model(X_train, Y_train, X_test, n_estimator, expert_model, _DS, _LP):
     models = []
     if expert_model == "DT":
         models = [DecisionTreeClassifier() for i in range(n_estimator)]
     elif expert_model == "MLP":
         models = [MLPClassifier() for i in range(n_estimator)]
+    elif expert_model == "NB":
+        models = [GaussianNB() for i in range(n_estimator)]
+    elif expert_model == "svm":
+        models = [svm.SVC() for i in range(n_estimator)]
 
-    gm = GaussianMixture(n_components = n_estimator, init_params = 'random')
+    gm = GaussianMixture(n_components=n_estimator, init_params='random')
     gm.fit(X_train)
     train_prob = gm.predict_proba(X_train)
 
-    x = [ None ] * n_estimator
-    y = [ None ] * n_estimator
-    del_e = [ None ] * n_estimator
+    x = [None] * n_estimator
+    y = [None] * n_estimator
+    del_e = [None] * n_estimator
 
     for j in range(n_estimator):
         del_e[j] = np.ndarray.tolist(np.arange(X_train.shape[0]))
         x[j] = X_train
         y[j] = Y_train
-
 
     for i in range(X_train.shape[0]):
         for j in range(n_estimator):
@@ -124,27 +139,28 @@ def ME_model( X_train, Y_train, X_test, n_estimator, expert_model, _DS, _LP):
                 del_e[j].remove(i)
 
     for j in range(n_estimator):
-        x[j] = np.delete(x[j],del_e[j],0)
-        y[j] = np.delete(y[j],del_e[j],0)
-    #Done soft split of train data
+        x[j] = np.delete(x[j], del_e[j], 0)
+        y[j] = np.delete(y[j], del_e[j], 0)
+    # Done soft split of train data
 
-    #Training Model
+    # Training Model
     for j in range(n_estimator):
-        models[j].fit(x[j],y[j])
+        models[j].fit(x[j], y[j])
 
-    #Testing model
+    # Testing model
     test_prob = gm.predict_proba(X_test)
     Y_pred = np.array([0 for _ in range(X_test.shape[0])])
     for i in range(X_test.shape[0]):
         temp = 0
         for j in range(n_estimator):
-            y = models[j].predict(X_test[i:i+1, :])
+            y = models[j].predict(X_test[i:i + 1, :])
             temp += test_prob[i][j] * y
         if temp > _LP:
             Y_pred[i] = 1
     return Y_pred
 
-#Unsucessfull experiment
+
+# Unsucessfull experiment
 """
 def UnsupervisedMixtureofExperts(X_train,Y_train,X_test,n_estimators=2):
 
@@ -162,7 +178,7 @@ def UnsupervisedMixtureofExperts(X_train,Y_train,X_test,n_estimators=2):
     print(tr_prob)
     #train_prob = convert_vec2matrix(tr_prob,n_estimators)
     #print(train_prob)
-    
+
     x = [ None ] * n_estimators
     y = [ None ] * n_estimators
     del_e = [ None ] * n_estimators
@@ -206,41 +222,101 @@ def UnsupervisedMixtureofExperts(X_train,Y_train,X_test,n_estimators=2):
     return Y_pred
 """
 
-#Bagging ensemble
+
+# Bagging ensemble
 def bag_model(X_train, Y_train, X_test, expert_model, n_estimator):
-
     if expert_model == "DT":
-        model = BaggingClassifier( DecisionTreeClassifier(),
-                                   n_estimators = n_estimator,
-                                   max_samples = 0.7,
-                                   max_features = 0.7,
-                                   bootstrap = True,
-                                   bootstrap_features = True)
+        model = BaggingClassifier(DecisionTreeClassifier(),
+                                  n_estimators=n_estimator,
+                                  max_samples=0.7,
+                                  max_features=0.7,
+                                  bootstrap=True,
+                                  bootstrap_features=True)
     if expert_model == "MLP":
-        model = BaggingClassifier( MLPClassifier(),
-                                   n_estimators = n_estimator,
-                                   max_samples = 0.7,
-                                   max_features = 0.7,
-                                   bootstrap = True,
-                                   bootstrap_features = True)
+        model = BaggingClassifier(MLPClassifier(),
+                                  n_estimators=n_estimator,
+                                  max_samples=0.7,
+                                  max_features=0.7,
+                                  bootstrap=True,
+                                  bootstrap_features=True)
+    if expert_model == "NB":
+        model = BaggingClassifier(GaussianNB(),
+                                  n_estimators=n_estimator,
+                                  max_samples=0.7,
+                                  max_features=0.7,
+                                  bootstrap=True,
+                                  bootstrap_features=True)
+    if expert_model == "svm":
+        model = BaggingClassifier(svm.SVC(),
+                                  n_estimators=n_estimator,
+                                  max_samples=0.7,
+                                  max_features=0.7,
+                                  bootstrap=True,
+                                  bootstrap_features=True)
 
-    model.fit(X_train ,Y_train)
+    model.fit(X_train, Y_train)
 
     Y_pred = model.predict(X_test)
 
     return Y_pred
 
-#Single/Individual Model
+
+# Boosting ensemble
+def boost_model(X_train, Y_train, X_test, expert_model, n_estimator):
+    if expert_model == "DT":
+        model = AdaBoostClassifier(DecisionTreeClassifier(),
+                                   n_estimators=n_estimator,
+                                   algorithm='SAMME.R')
+    if expert_model == "MLP":
+        model = AdaBoostClassifier(MLPClassifier(),
+                                   n_estimators=n_estimator,
+                                   algorithm='SAMME.R')
+    if expert_model == "NB":
+        model = AdaBoostClassifier(GaussianNB(),
+                                   n_estimators=n_estimator,
+                                   algorithm='SAMME.R')
+    if expert_model == "svm":
+        model = AdaBoostClassifier(svm.SVC(),
+                                   n_estimators=n_estimator,
+                                   algorithm='SAMME.R')
+
+    model.fit(X_train, Y_train)
+
+    Y_pred = model.predict(X_test)
+
+    return Y_pred
+
+
+# Stacking Ensemble
+def stack_model(X_train, Y_train, X_test, expert_model, n_estimator):
+    estimators = [('DT', DecisionTreeClassifier()),
+                  ('MLP', MLPClassifier())]
+    if expert_model == "DT":
+        model = StackingClassifier(estimators=estimators, final_estimator=DecisionTreeClassifier())
+    if expert_model == "MLP":
+        model = StackingClassifier(estimators=estimators, final_estimator=MLPClassifier())
+    model.fit(X_train, Y_train)
+
+    Y_pred = model.predict(X_test)
+
+    return Y_pred
+
+
+# Single/Individual Model
 def indi_model(X_train, Y_train, X_test, expert_model):
     print()
-    print("Individual Model: "+expert_model)
+    print("Individual Model: " + expert_model)
     print()
     if expert_model == "DT":
         model = DecisionTreeClassifier()
     if expert_model == "MLP":
         model = MLPClassifier()
+    if expert_model == "NB":
+        model = GaussianNB()
+    if expert_model == "svm":
+        model = svm.SVC()
 
-    model.fit(X_train ,Y_train)
+    model.fit(X_train, Y_train)
 
     Y_pred = model.predict(X_test)
 
--- tester.py
+++ tester.py
@@ -2,39 +2,44 @@ import os
 import time
 import statistics
 import numpy as np
-import helpers as hlp   #helpers.py requires
+import helpers as hlp  # helpers.py requires
 import pandas as pd
 from sklearn.model_selection import KFold
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 import sys
 
-#Reading arguments passed from wrapper.py
+print("we are in tester")
+
+# cwd = os.getcwd()
+# Reading arguments passed from wrapper.py
 dataset_name = sys.argv[1]
 expert_model = sys.argv[2]
 agg_type = sys.argv[3]
 LP_threshold = float(sys.argv[4])
 DS_threshold = float(sys.argv[5])
+print(dataset_name, expert_model, agg_type, LP_threshold, DS_threshold)
 
 cwd = os.getcwd()
 
-#'Results' folder should be in same directory
-#Output will be stored in 'Results' folder in file like 'CM1_ME_DT.csv'
-outfile = open("Results/" + dataset_name + "_" + expert_model + "_" + agg_type +".csv" , "a+")
-
-#Sampling required
+# 'Results' folder should be in same directory
+# Output will be stored in 'Results' folder in file like 'CM1_ME_DT.csv'
+outfile = open(cwd + "/Results/" + dataset_name + "_" + expert_model + "_" + agg_type + ".csv", "a+")
+# outfile = open(cwd + dataset_name + "_" + expert_model + "_" + agg_type +".csv" , "a+")
+# print(outfile)
+# Sampling required
 sr = "yes"
-#Standardization
+# Standardization
 st = "yes"
-#Feature selection using PCA
+# Feature selection using PCA
 pr = "no"
-#normailzation
+# normailzation
 nr = "no"
 
-X,Y,color = hlp.loaddata(dataset_name = dataset_name,
-                   sampling_required = sr,
-                   standardisation = st,
-                   pca_reduced = pr,
-                   normalization = nr)
+X, Y, color = hlp.loaddata(dataset_name=dataset_name,
+                           sampling_required=sr,
+                           standardisation=st,
+                           pca_reduced=pr,
+                           normalization=nr)
 
 print("Data loaded with:", end=" ")
 if sr == "yes":
@@ -46,16 +51,26 @@ if pr == "yes":
 if nr == "yes":
     print("Normalization", end=" ")
 
-outfile.write("DS_threshold,LP_threshold,Accuracy,f1-score,Precision,Recall,Time(secs)\n")
+# outfile.write("DS_threshold,LP_threshold,Accuracy,f1-score,Precision,Recall,AUC,Time(secs),TP,FP,FN,TN\n")
 
-#Number of base learners = 10
-for n in range(10,11,2):
+# Number of base learners = 10
+for n in range(10, 30, 2):
+    # for n in range(10):
     start_time = time.time()
-    kf = KFold(n_splits=5, random_state=None, shuffle=True) #5-fold CV
+    kf = KFold(n_splits=5, random_state=None, shuffle=True)  # 5-fold CV
     acc = []
     prec = []
     rec = []
     f1 = []
+    AUC = []
+    # TP= []
+    # TN= []
+    # FP= []
+    # FN= []
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
 
     for train_index, test_index in kf.split(X):
         X_train, X_test = X[train_index], X[test_index]
@@ -63,32 +78,57 @@ for n in range(10,11,2):
 
         if agg_type == "ME":
             Y_pred = hlp.ME_model(X_train, Y_train, X_test,
-                                  n_estimator = n,
-                                  expert_model = expert_model,
-                                  _DS = DS_threshold,
-                                  _LP = LP_threshold)
-        
+                                  n_estimator=n,
+                                  expert_model=expert_model,
+                                  _DS=DS_threshold,
+                                  _LP=LP_threshold)
+
         elif agg_type == "bag":
-             Y_pred = hlp.bag_model(X_train, Y_train, X_test,
-                                    n_estimator = n,
-                                    expert_model = expert_model)
+            Y_pred = hlp.bag_model(X_train, Y_train, X_test,
+                                   n_estimator=n,
+                                   expert_model=expert_model)
 
         elif agg_type == "indi":
-             Y_pred = hlp.indi_model(X_train, Y_train, X_test,
-                                     expert_model = expert_model)
-             
+            Y_pred = hlp.indi_model(X_train, Y_train, X_test,
+                                    expert_model=expert_model)
+
+        elif agg_type == "boost":
+            Y_pred = hlp.boost_model(X_train, Y_train, X_test,
+                                     n_estimator=n,
+                                     expert_model=expert_model)
+        elif agg_type == "stack":
+            Y_pred = hlp.stack_model(X_train, Y_train, X_test,
+                                     n_estimator=n,
+                                     expert_model=expert_model)
+
+        for i in range(len(Y_pred)):
+            if Y_pred[i] == Y_test[i] == 1:
+                TP += 1
+            if Y_pred[i] == 1 and Y_test[i] != Y_pred[i]:
+                FP += 1
+            if Y_test[i] == Y_pred[i] == 0:
+                TN += 1
+            if Y_pred[i] == 0 and Y_test[i] != Y_pred[i]:
+                FN += 1
         acc.append(accuracy_score(Y_test, Y_pred))
         prec.append(precision_score(Y_test, Y_pred, pos_label=1))
         rec.append(recall_score(Y_test, Y_pred, pos_label=1))
         f1.append(f1_score(Y_test, Y_pred, pos_label=1))
-   
+        AUC.append(roc_auc_score(Y_test, Y_pred))
+        # TP.append(TP)
+        # TN.append(TN)
+        # FP.append(FP)
+        # FN.append(FN)
     outfile.write(str(DS_threshold) +
                   "," + str(LP_threshold) +
-                  "," + str(round(statistics.mean(acc),4)) +
-                  "," + str(round(statistics.mean(f1),4)) +
-                  "," + str(round(statistics.mean(prec),4)) +
-                  "," + str(round(statistics.mean(rec),4)) +
-                  "," +  str(round(time.time()-start_time,3)) + "\n")
-
+                  "," + str(round(statistics.mean(acc), 4)) +
+                  "," + str(round(statistics.mean(f1), 4)) +
+                  "," + str(round(statistics.mean(prec), 4)) +
+                  "," + str(round(statistics.mean(rec), 4)) +
+                  "," + str(round(statistics.mean(AUC), 4)) +
+                  "," + str(round(time.time() - start_time, 3)) +
+                  "," + str(TP) +
+                  "," + str(FP) +
+                  "," + str(FN) +
+                  "," + str(TN) + "\n")
 outfile.close()
-
